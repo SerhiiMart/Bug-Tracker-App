568b8a721388c69f0b7b22022bcf545ed43e7fa5
Serhii <malgothsm@gmail.com>
2021-02-07 17:21:55 +0200

    Added switch in reducer

--- redux-starter/src/reducer.js
+++ redux-starter/src/reducer.js
@@ -1,15 +1,18 @@
 let lastId = 0;
 function reducer(state = [], action) {
-  if (action.type === 'bugAdded')
-  return [
-    ...state,
-    {
-      id: ++lastId,
-      desctiption: action.payload.desctiption,
-      resolved: false
-    }
-  ];
-  else if (action.type === 'bugRemoved')
-    return state.filter(bug => bug.id !== action.payload.id);
-  return state;
+  switch (action.type){
+    case 'bugAdded':
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          desctiption: action.payload.desctiption,
+          resolved: false
+        }
+      ];
+    case 'bugRemoved':
+      return state.filter(bug => bug.id !== action.payload.id);
+    default:
+      return state;
+  }  
 }
